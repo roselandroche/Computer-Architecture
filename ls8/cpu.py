6a93554d1ade408ecb48bfc50770b1a25bd5914e
roselandroche <r.landroche@gmail.com>
2020-09-02 21:05:03 -0400

    Working through Day 1 tasks

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,17 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [00000000] * 256 # memory to hold 256 bytes
+        self.reg = [0] * 8          # 8 all purpose registers
+        self.pc = 0                 # pc counter
+
+    def ram_read(self, address):
+        # accept the address to read and return the value stored there
+        return self.ram[address]
+
+    def ram_write(self, data, address):
+        # accept a value to write, and the address to write it to
+        self.ram[address] = data
 
     def load(self):
         """Load a program into memory."""
@@ -62,4 +72,31 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        # read the memory address that's stored in register PC, 
+            # and store that result in IR, the Instruction Register
+        instruction = self.ram[self.pc]
+
+        # Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into 
+            # variables operand_a and operand_b in case the instruction needs them.
+
+        operand_a = self.ram_read(self.pc + 1)
+        operand_b = self.ram_read(self.pc + 2)
+
+        # depending on the value of the opcode, perform the actions needed for the 
+            # instruction per the LS-8 spec. Maybe an if-elif cascade...?
+
+        if instruction == 0b10000010: # LDI R0,8
+            self.reg[operand_a] = operand_b
+            self.pc += 3
+        elif instruction == 0b01000111: # PRN R0
+            print(self.reg[operand_a])
+            self.pc += 2
+        elif instruction == 0b00000001: # HLT
+            self.pc += 1
+            return
+        else:
+            print(f'Unknown instruction: {instruction}')
+
+# first_try = CPU()
+# first_try.load()
+# first_try.run()
