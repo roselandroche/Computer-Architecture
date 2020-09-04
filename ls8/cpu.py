2a299e043998f958c815875a225b7ca69bd094ac
roselandroche <r.landroche@gmail.com>
2020-09-04 19:58:47 -0400

    Finished Day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -47,29 +47,14 @@ class CPU:
         except FileNotFoundError:
             print(f'{sys.argv[1]} not found')
 
-        # For now, we've just hardcoded a program:
-
-        # program = [
-            # From print8.ls8
-            # 0b10000010, # LDI R0,8
-            # 0b00000000,
-            # 0b00001000,
-            # 0b01000111, # PRN R0
-            # 0b00000000,
-            # 0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == 0b10100010:        # MUL R0,R1
+            product = self.reg[reg_a] * self.reg[reg_b]
+            self.reg[reg_a] = product
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -118,5 +103,8 @@ class CPU:
             elif instruction == 0b00000001:    # HLT
                 self.pc += 1
                 self.running = False
+            elif instruction == 0b10100010:      # MUL R0,R1
+                self.alu(instruction, operand_a, operand_b)
+                self.pc += 3
             else:
                 print(f'Unknown instruction: {instruction}')
